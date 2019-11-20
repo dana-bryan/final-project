6a04e2f118561eddfb0966b9dc1c509778bc1619
dana-bryan <dana-bryan@users.noreply.github.com>
2019-11-19 22:05:29 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('prom').addEventListener("click", function() {
+	document.querySelector('#proandpre').style.display = "flex";
+});
+
+document.querySelector('.close').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
